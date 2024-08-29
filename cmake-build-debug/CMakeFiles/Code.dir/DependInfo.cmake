5e5c1f387dda16bf1d24e42b54d7e92a1f15841a
OstrvskVld <vldstrvskj@gmail.com>
2024-08-30 01:26:25 +0300

    Added git.ignore

---
+++ cmake-build-debug/CMakeFiles/Code.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Course Work/Code/Bike.cpp" "CMakeFiles/Code.dir/Bike.cpp.obj" "gcc" "CMakeFiles/Code.dir/Bike.cpp.obj.d"
+  "D:/Course Work/Code/Cars.cpp" "CMakeFiles/Code.dir/Cars.cpp.obj" "gcc" "CMakeFiles/Code.dir/Cars.cpp.obj.d"
+  "D:/Course Work/Code/Drivers.cpp" "CMakeFiles/Code.dir/Drivers.cpp.obj" "gcc" "CMakeFiles/Code.dir/Drivers.cpp.obj.d"
+  "D:/Course Work/Code/Engine.cpp" "CMakeFiles/Code.dir/Engine.cpp.obj" "gcc" "CMakeFiles/Code.dir/Engine.cpp.obj.d"
+  "D:/Course Work/Code/Functions.cpp" "CMakeFiles/Code.dir/Functions.cpp.obj" "gcc" "CMakeFiles/Code.dir/Functions.cpp.obj.d"
+  "D:/Course Work/Code/People.cpp" "CMakeFiles/Code.dir/People.cpp.obj" "gcc" "CMakeFiles/Code.dir/People.cpp.obj.d"
+  "D:/Course Work/Code/Sedan.cpp" "CMakeFiles/Code.dir/Sedan.cpp.obj" "gcc" "CMakeFiles/Code.dir/Sedan.cpp.obj.d"
+  "D:/Course Work/Code/Users.cpp" "CMakeFiles/Code.dir/Users.cpp.obj" "gcc" "CMakeFiles/Code.dir/Users.cpp.obj.d"
+  "D:/Course Work/Code/Van.cpp" "CMakeFiles/Code.dir/Van.cpp.obj" "gcc" "CMakeFiles/Code.dir/Van.cpp.obj.d"
+  "D:/Course Work/Code/Vehicle.cpp" "CMakeFiles/Code.dir/Vehicle.cpp.obj" "gcc" "CMakeFiles/Code.dir/Vehicle.cpp.obj.d"
+  "D:/Course Work/Code/main.cpp" "CMakeFiles/Code.dir/main.cpp.obj" "gcc" "CMakeFiles/Code.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/Bike.cpp.obj"
+  "CMakeFiles/Code.dir/Bike.cpp.obj.d"
+  "CMakeFiles/Code.dir/Cars.cpp.obj"
+  "CMakeFiles/Code.dir/Cars.cpp.obj.d"
+  "CMakeFiles/Code.dir/Drivers.cpp.obj"
+  "CMakeFiles/Code.dir/Drivers.cpp.obj.d"
+  "CMakeFiles/Code.dir/Engine.cpp.obj"
+  "CMakeFiles/Code.dir/Engine.cpp.obj.d"
+  "CMakeFiles/Code.dir/Functions.cpp.obj"
+  "CMakeFiles/Code.dir/Functions.cpp.obj.d"
+  "CMakeFiles/Code.dir/People.cpp.obj"
+  "CMakeFiles/Code.dir/People.cpp.obj.d"
+  "CMakeFiles/Code.dir/Sedan.cpp.obj"
+  "CMakeFiles/Code.dir/Sedan.cpp.obj.d"
+  "CMakeFiles/Code.dir/Users.cpp.obj"
+  "CMakeFiles/Code.dir/Users.cpp.obj.d"
+  "CMakeFiles/Code.dir/Van.cpp.obj"
+  "CMakeFiles/Code.dir/Van.cpp.obj.d"
+  "CMakeFiles/Code.dir/Vehicle.cpp.obj"
+  "CMakeFiles/Code.dir/Vehicle.cpp.obj.d"
+  "CMakeFiles/Code.dir/main.cpp.obj"
+  "CMakeFiles/Code.dir/main.cpp.obj.d"
+  "Code.exe"
+  "Code.exe.manifest"
+  "Code.pdb"
+  "libCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
